e5850ad34c3a4e6cbc6cd7b506ff812f71b1e375
jwqe764241 <jwqe764241@gmail.com>
2021-07-23 15:45:50 +0900

    Add to exclude layout from several paths

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 /* eslint-disable */
 import "styles/reset.css";
 import "styles/global.css";
+import App from "next/app";
 import styled from "styled-components";
-
 import Navbar from "components/layout/Navbar";
+import { Component } from "react";
 
 const Layout = styled.div`
   display: flex;
@@ -11,15 +12,36 @@ const Layout = styled.div`
   height: 100%;
 `;
 
-function App({ Component, pageProps }) {
-  return (
+const excludePath = new Set([
+  "/",
+  "/_error"
+])
+
+function MyApp({ Component, pageProps }) {
+  const pathname = pageProps && pageProps.pathname;
+  return excludePath.has(pathname) ? (
+    <>
+      <Component {...pageProps} />
+    </>
+  ) : (
     <>
       <Layout>
         <Navbar />
         <Component {...pageProps} />
       </Layout>
     </>
-  );
+  )
 }
 
-export default App;
+MyApp.getInitialProps = async (appContext) => {
+  const {ctx, Component} = appContext;
+  let pageProps = {};
+
+  if(Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return { pageProps : {...pageProps, pathname: ctx.pathname} };
+}
+
+export default MyApp;
