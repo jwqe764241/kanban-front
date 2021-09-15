710183b3e5f3f2d82fc0669b16396d83403d069b
jwqe764241 <jwqe764241@gmail.com>
2021-09-15 21:15:56 +0900

    Add invitation accept url

---
+++ pages/projects/[id]/invitation.js
@@ -0,0 +1,44 @@
+import { connect, useSelector, useDispatch } from "react-redux";
+import wrapper from "core/store";
+import { parseCookie } from "core/utils";
+import axios, { createRequester } from "core/apiAxios";
+
+const Invitation = () => {
+  return <></>;
+};
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async (context) => {
+    const { id } = context.query;
+    const cookies = parseCookie(context.req.headers.cookie);
+    const requester = createRequester(
+      axios,
+      store.dispatch,
+      cookies.REFRESH_TOKEN,
+    );
+
+    const { token } = store.getState();
+    try {
+      const acceptResponse = await requester.post(
+        `/projects/${id}/invitation`,
+        null,
+        token,
+      );
+      return {
+        redirect: {
+          destination: `/projects/${id}/kanbans`,
+          permanent: false,
+        },
+      };
+    } catch (e) {
+      return {
+        redirect: {
+          destination: `/`,
+          permanent: false,
+        },
+      };
+    }
+  },
+);
+
+export default connect((state) => state)(Invitation);
