754bfa34a4e5bd608ce09cd77b73f311f116abf0
jwqe764241 <jwqe764241@gmail.com>
2021-10-05 15:36:55 +0900

    Add kanban list

--- pages/projects/[id]/kanbans/index.js
+++ pages/projects/[id]/kanbans/index.js
@@ -1,21 +1,88 @@
 import { connect } from "react-redux";
+import { useRouter } from "next/router";
+import styled from "styled-components";
 import PropTypes from "prop-types";
+import Link from "next/link";
 import wrapper from "core/store";
-import { parseCookie } from "core/utils";
+import { parseCookie, getDateString } from "core/utils";
 import axios, { createRequester } from "core/apiAxios";
 
 import ProjectHeader from "components/project/ProjectHeader";
+import { ContainerXL } from "components/layout/Container";
+import { SuccessButton } from "components/layout/Button";
 
-const Kanbans = ({ project }) => {
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const KanbanInfo = styled.div`
+  padding: 24px 0px;
+  border-bottom: 1px solid #e1e4e8;
+  color: #212427;
+
+  &:first-child {
+    padding-top: 0px;
+  }
+`;
+
+const KanbanName = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+  color: #2455e7;
+  margin-bottom: 10px;
+  cursor: pointer;
+
+  &: hover {
+    text-decoration: underline;
+  }
+`;
+
+const KanbanDescription = styled.div`
+  font-size: 14px;
+  font-weight: 400;
+  margin-bottom: 20px;
+  color: #484848;
+`;
+
+const RegisterDate = styled.div`
+  font-size: 12px;
+  font-weight: 300;
+  margin-top: 10px;
+  color: #6a737d;
+`;
+
+const KanbanList = ({ project, kanbanList }) => {
+  const router = useRouter();
+  const { id } = router.query;
+
   return (
     <>
       <ProjectHeader project={project} activeMenu="kanbans" />
-      <div>xcvxcvxcvxcv</div>
+      <ContainerXL>
+        <Container>
+          <div />
+          <Link href={`/projects/${id}/kanbans/new`}>
+            <SuccessButton style={{ width: "120px" }}>New Kanban</SuccessButton>
+          </Link>
+        </Container>
+        {kanbanList.map(
+          ({ projectId, sequenceId, name, description, registerDate }) => (
+            <KanbanInfo key={sequenceId}>
+              <Link href={`/projects/${projectId}/kanbans/${sequenceId}`}>
+                <KanbanName>{name}</KanbanName>
+              </Link>
+              <KanbanDescription>{description}</KanbanDescription>
+              <RegisterDate>{getDateString(registerDate)}</RegisterDate>
+            </KanbanInfo>
+          ),
+        )}
+      </ContainerXL>
     </>
   );
 };
 
-Kanbans.propTypes = {
+KanbanList.propTypes = {
   project: PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string,
@@ -23,6 +90,7 @@ Kanbans.propTypes = {
     registerUsername: PropTypes.string,
     registerDate: PropTypes.string,
   }).isRequired,
+  kanbanList: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export const getServerSideProps = wrapper.getServerSideProps(
@@ -37,13 +105,19 @@ export const getServerSideProps = wrapper.getServerSideProps(
 
     const { token } = store.getState();
     try {
-      const response = await requester.get(`/projects/${id}`, token);
+      const [projectResponse, kanbanResponse] = await Promise.all([
+        requester.get(`/projects/${id}`, token),
+        requester.get(`/projects/${id}/kanbans`, token),
+      ]);
+
       return {
         props: {
-          project: response.data,
+          project: projectResponse.data,
+          kanbanList: kanbanResponse.data,
         },
       };
     } catch (e) {
+      console.log(e);
       return {
         notFound: true,
       };
@@ -51,4 +125,4 @@ export const getServerSideProps = wrapper.getServerSideProps(
   },
 );
 
-export default connect((state) => state)(Kanbans);
+export default connect((state) => state)(KanbanList);
