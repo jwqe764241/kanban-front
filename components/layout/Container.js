c200897995866f775bfa05bc48f925a229284122
jwqe764241 <jwqe764241@gmail.com>
2021-12-21 14:07:09 +0900

    Update Container to fit screen

--- components/layout/Container.js
+++ components/layout/Container.js
@@ -1,21 +1,22 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  box-sizing: content-box;
+  box-sizing: border-box;
   margin: 40px auto;
   padding: 0px 30px;
+  width: 100%;
 `;
 
 const ContainerMd = styled(Container)`
-  width: 720px;
+  max-width: 720px;
 `;
 
 const ContainerLg = styled(Container)`
-  width: 960px;
+  max-width: 960px;
 `;
 
 const ContainerXL = styled(Container)`
-  width: 1280px;
+  max-width: 1280px;
 `;
 
 export { Container, ContainerMd, ContainerLg, ContainerXL };
