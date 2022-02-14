adb083c0c8436799738ee3366c34d4b89e44c033
jwqe764241 <jwqe764241@gmail.com>
2022-02-14 17:55:03 +0900

    Add svg webpack plugin

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
