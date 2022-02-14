c36ec87da3ef6cb5af32b63cf10e8bfaf50f6201
jwqe764241 <jwqe764241@gmail.com>
2022-02-14 14:24:50 +0900

    Move global style to component

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,41 @@
 /* eslint-disable */
-import "styles/reset.css";
-import "styles/global.css";
+import GlobalStyle from "components/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import Theme from "components/Theme";
 import wrapper from "core/store";
 import { getCookie, parseJwtClaims } from "core/utils";
 
 import Navbar from "components/layout/Navbar";
 
-const excludePath = new Set([
-  "/login",
-  "/_error"
-])
+const excludePath = new Set(["/login", "/_error"]);
 
 function MyApp({ Component, pageProps }) {
   const { pathname, auth } = pageProps;
 
   return excludePath.has(pathname) ? (
     <>
-      <Component {...pageProps} />
-      <div id="modal-root" />
+      <GlobalStyle />
+      <ThemeProvider theme={Theme}>
+        <Component {...pageProps} />
+        <div id="modal-root" />
+      </ThemeProvider>
     </>
   ) : (
     <>
-      <Navbar name={auth?.name}/>
-      <Component {...pageProps} />
-      <div id="modal-root" />
+      <GlobalStyle />
+      <ThemeProvider theme={Theme}>
+        <Navbar name={auth?.name} />
+        <Component {...pageProps} />
+        <div id="modal-root" />
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
 MyApp.getInitialProps = async (appContext) => {
-  const {ctx, Component} = appContext;
-  let pageProps = {
-    pathname: ctx.pathname
+  const { ctx, Component } = appContext;
+  const pageProps = {
+    pathname: ctx.pathname,
   };
 
   const refreshToken = getCookie("REFRESH_TOKEN", ctx.req);
@@ -43,12 +47,12 @@ MyApp.getInitialProps = async (appContext) => {
     };
   }
 
-  if(Component.getInitialProps) {
+  if (Component.getInitialProps) {
     const props = await Component.getInitialProps(ctx);
     Object.assign(pageProps, props);
   }
 
   return { pageProps };
-}
+};
 
 export default wrapper.withRedux(MyApp);
