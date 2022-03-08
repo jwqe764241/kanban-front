27657571f07041062ef7cffc5c12b0e0cffbc2d6
jwqe764241 <jwqe764241@gmail.com>
2022-03-08 18:03:01 +0900

    Fix bug that new css class is generated when context menu shown

--- components/kanban/ContextMenuContainer.js
+++ components/kanban/ContextMenuContainer.js
@@ -5,15 +5,18 @@ import PropTypes from "prop-types";
 import ContextMenuPortal from "./ContextMenuPortal";
 import ContextMenuContext from "./ContextMenuContext";
 
-const Container = styled.div`
+const Container = styled.div.attrs(({ position }) => ({
+  style: {
+    top: position ? `${position.y}px` : "0px",
+    left: position ? `${position.x}px` : "0px",
+  },
+}))`
   position: absolute;
   background-color: ${({ theme }) => theme.colors.white};
   border-radius: 4px;
   box-shadow: rgb(100 100 111 / 25%) 0px 0px 20px 5px;
   width: 200px;
   z-index: 100;
-  ${({ position }) =>
-    position ? `top: ${position.y}px; left: ${position.x}px;` : ""}
 `;
 
 const ContextMenuContainer = ({ id, children }) => {
